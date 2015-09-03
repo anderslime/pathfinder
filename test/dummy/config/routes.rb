58e895cb4880935294e3d7b406f73d3f1dcd3746
Anders Emil Nielsen <aemilnielsen@gmail.com>
2015-09-03 10:23:24 +0200

    Integration tests for pathfinder

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
 
+  get '/news' => 'news#index'
+
+  namespace :hello do
+    post '/world' => "hello_world#index"
+  end
+
+  resources :locations
+
+  get '/wildcard/*/route' => 'wildcards#index'
+
   mount Pathfinder::Engine => "/pathfinder"
 end
